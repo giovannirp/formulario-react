19bb8898478a8c99581fe94b88ff70e5dc189792
Giovanni Ribeiro Pinto <giovanni.ribeiro@vertem.com>
2020-10-28 19:54:17 -0300

    fix: alterando layout novo

--- form-react/src/App.js
+++ form-react/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component, Fragment } from 'react';
 import './App.css';
 import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+import { Container, Typography } from "@material-ui/core"
+import 'fontsource-roboto';
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <h1>Formulario de Cadastro</h1>
+      <Container component="article" maxWidth="sm">
+        <Typography variant="h3" component="h1" color="primary" align="center">Formulario de Cadastro</Typography>
         <FormularioCadastro />
-      </Fragment>
+      </Container>
     );
   }
 }
--- form-react/src/components/FormularioCadastro/FormularioCadastro.js
+++ form-react/src/components/FormularioCadastro/FormularioCadastro.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import Button from "@material-ui/core/Button";
+import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core"
 
 function FormularioCadastro() {
     return(
         <form>
-            <label>Nome: </label>
-            <input type="text" />
+            <TextField id="nome" label="Nome" variant="outlined" margin="normal" fullWidth />
 
-            <label>Sobrenome: </label>
-            <input type="text" />
+            <TextField id="sobreNome" label="sobreNome" variant="outlined" margin="normal" fullWidth />
 
-            <label>CPF: </label>
-            <input type="text" />
-
-            <label>Promoções: </label>
-            <input type="checkbox" />
-
-            <label>Novidades: </label>
-            <input type="checkbox" />
+            <TextField id="CPF" label="sobreNome" variant="outlined" margin="normal" fullWidth />
+            
+            <FormControlLabel 
+                label="Promoçoes"
+                control={
+                    <Switch name="promocoes" defaultChecked color="primary" />
+                } 
+            />
+            
+            <FormControlLabel 
+                label="Novidades"
+                control={
+                    <Switch name="novidades" defaultChecked color="primary" />
+                }
+            />
 
             <Button type="submit" variant="contained" color="primary">
                 Cadastrar
